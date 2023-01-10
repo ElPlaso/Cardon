29f48fc10fdcf303c52bbc5c5ba542bd36abd70f
Plaso Kusay <plaso.kusay@manfrotto.com>
2023-01-11 09:49:21 +1300

    homepage: replace buttons with bottom app bar.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,7 +8,6 @@ import '../widgets/banner.dart';
 import 'scan.dart';
 import 'user_cards.dart';
 import 'wallet.dart';
-import '../widgets/logo_button.dart';
 import '../widgets/carousel.dart';
 
 // * Homepage that users are redirected to when logged in
@@ -19,10 +18,10 @@ class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  _HomeState createState() => _HomeState();
+  HomeState createState() => HomeState();
 }
 
-class _HomeState extends State<Home> {
+class HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
@@ -33,61 +32,79 @@ class _HomeState extends State<Home> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) => Scaffold(
-        key: scaffoldKey,
-        appBar: AppBar(
-          title: const Text(MyApp.title),
-          centerTitle: true,
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: const Text(MyApp.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            context.watch<Cards>().isEmpty(true)
+                ? const HomeBanner(subheading: 'Add a card to get started :)')
+                : Carousel(),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              context.watch<Cards>().isEmpty(true)
-                  ? const HomeBanner(subheading: 'Add a card to get started :)')
-                  : Carousel(),
-              LogoButton(
-                text: 'Scan',
-                onClicked: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Scan()),
-                ),
-                icon: const Icon(Icons.qr_code_scanner, size: 40),
-              ),
-              LogoButton(
-                text: 'Create',
-                onClicked: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const UserCards()),
-                ),
-                icon: const Icon(Icons.library_add, size: 40),
-              ),
-              LogoButton(
-                text: 'View Wallet',
-                onClicked: () => {
-                  if (context.read<Cards>().isEmpty(false))
-                    {
-                      Fluttertoast.showToast(
-                          msg:
-                              "No saved cards. Scan a card to add it to your list!",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.CENTER,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.blue,
-                          textColor: Colors.white,
-                          fontSize: 16.0)
-                    }
-                  else
-                    {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Wallet()),
-                      )
-                    }
-                },
-                icon: const Icon(Icons.wallet, size: 40),
-              ),
-            ],
-          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Floating action button on Scaffold
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Scan()),
         ),
-      );
+        child:
+            const Icon(Icons.crop_free_outlined, size: 35), //icon inside button
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        //bottom navigation bar on scaffold
+        color: Colors.blue,
+        shape: const CircularNotchedRectangle(), //shape of notch
+        notchMargin:
+            5, //notche margin between floating button and bottom appbar
+        child: Row(
+          //children inside bottom appbar
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.library_add,
+                color: Colors.white,
+              ),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const UserCards()),
+              ),
+            ),
+            IconButton(
+                icon: const Icon(
+                  Icons.wallet,
+                  color: Colors.white,
+                ),
+                onPressed: () => {
+                      if (context.read<Cards>().isEmpty(false))
+                        {
+                          Fluttertoast.showToast(
+                              msg:
+                                  "No saved cards. Scan a card to add it to your list!",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.blue,
+                              textColor: Colors.white,
+                              fontSize: 16.0)
+                        }
+                      else
+                        {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Wallet()),
+                          )
+                        }
+                    }),
+          ],
+        ),
+      ));
 }
