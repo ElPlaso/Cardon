408965945b830ad5322f0ce380022b3abf2aaacc
Moo Plah Soe Kusay <kusaymoo@ecs.vuw.ac.nz>
2023-01-15 22:51:36 +1300

    card_list: update layout of user cards page.

--- lib/pages/user_cards.dart
+++ lib/pages/user_cards.dart
@@ -1,7 +1,7 @@
+import 'package:cardonapp/widgets/tapped_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:cardonapp/widgets/card_stack.dart';
 import 'add_card.dart';
-import '../widgets/floater.dart';
 
 // * Page to display user's created cards
 // * Allows user to then create a new card
@@ -10,27 +10,42 @@ class UserCards extends StatelessWidget {
   const UserCards({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Your Cards'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 24),
-              const CardStack(),
-              // Navigates to the add card page
-              Floater(
-                  text: 'Add Card',
-                  onClicked: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AddCard()),
-                      ),
-                  icon: const Icon(Icons.add))
-            ],
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            scrolledUnderElevation: 5,
+            elevation: 0,
+            backgroundColor: Colors.grey[50],
+            title: const Text(
+              "Your Cards.",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 13),
+              child: TappedTextButton(
+                text: "",
+                iconData: Icons.chevron_left,
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            foregroundColor: Theme.of(context).colorScheme.primary,
+          ),
+          body: const Center(
+            child: CardStack(),
+          ),
+          floatingActionButton: FloatingActionButton(
+            // Floating action button on Scaffold.
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddCard()),
+            ),
+            child: const Icon(
+              Icons.library_add,
+              size: 25,
+            ),
           ),
         ),
       );
