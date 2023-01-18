f3c1c3d1b47f829dc8bef8c2c04c46a3244b3ee8
Moo Plah Soe Kusay <kusaymoo@ecs.vuw.ac.nz>
2023-01-18 23:49:49 +1300

    card_list: update card view.
    make sized box depend on number of cards.
    add scroll bar.
    fill empty space with text if no cards.

--- lib/pages/user_cards.dart
+++ lib/pages/user_cards.dart
@@ -1,6 +1,9 @@
+import 'package:cardonapp/providers/card_provider.dart';
 import 'package:cardonapp/widgets/tapped_text_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cardonapp/widgets/card_stack.dart';
+import 'package:provider/provider.dart';
 import 'add_card.dart';
 
 // * Page to display user's created cards
@@ -33,10 +36,40 @@ class UserCards extends StatelessWidget {
             ),
             foregroundColor: Theme.of(context).colorScheme.primary,
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [CardStack()],
-          ),
+          body: context.watch<Cards>().isEmpty(true)
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.browser_not_supported,
+                        size: 50,
+                      ),
+                      Text(
+                        "No Cards",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                )
+              : SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: CupertinoScrollbar(
+                    thumbVisibility: true,
+                    thickness: 5,
+                    radius: const Radius.circular(20),
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(
+                          parent: AlwaysScrollableScrollPhysics()),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [SizedBox(height: 20), CardStack()],
+                      ),
+                    ),
+                  ),
+                ),
           floatingActionButton: FloatingActionButton(
             // Floating action button on Scaffold.
             onPressed: () => Navigator.push(
--- lib/widgets/card_stack.dart
+++ lib/widgets/card_stack.dart
@@ -18,91 +18,33 @@ class CardStack extends StatefulWidget {
 }
 
 class CardStackState extends State<CardStack> {
+  final double cardHeight = 200;
+
   @override
   Widget build(BuildContext context) {
     final cardList = buildCardList(context);
-    return SingleChildScrollView(
-      physics:
-          const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height / 2,
-        width: MediaQuery.of(context).size.width,
-        child: CardStackWidget(
-          opacityChangeOnDrag: true,
-          swipeOrientation: CardOrientation.down,
-          cardDismissOrientation: CardOrientation.down,
-          positionFactor: 3,
-          scaleFactor: 0,
-          alignment: Alignment.topCenter,
-          animateCardScale: false,
-          cardList: cardList,
-        ),
+    final int numCards = context.watch<Cards>().personalcards.length;
+    return SizedBox(
+      height: cardHeight + (numCards) * 0.3 * cardHeight,
+      width: MediaQuery.of(context).size.width,
+      child: CardStackWidget(
+        opacityChangeOnDrag: true,
+        swipeOrientation:
+            numCards > 1 ? CardOrientation.both : CardOrientation.none,
+        cardDismissOrientation:
+            numCards > 1 ? CardOrientation.both : CardOrientation.none,
+        positionFactor: 3,
+        scaleFactor: 0,
+        alignment: Alignment.center,
+        animateCardScale: false,
+        cardList: cardList,
       ),
     );
   }
-  // LayoutBuilder(
-  //   builder: (context, constraints) => ListView(
-  //     physics: const BouncingScrollPhysics(
-  //         parent: AlwaysScrollableScrollPhysics()),
-  //     children: [
-  //       Container(
-  //         constraints: BoxConstraints(
-  //           minHeight: constraints.maxHeight,
-  //         ),
-  //         child: Padding(
-  //           padding: const EdgeInsets.only(bottom: 150),
-  //           child: Column(
-  //             mainAxisAlignment: MainAxisAlignment.center,
-  // children: context.watch<Cards>().personalcards.map(
-  //   (card) {
-  //     return Builder(
-  //       builder: (BuildContext context) {
-  //         return Align(
-  //           // Stack effect
-  //           heightFactor: 0.5,
-  //           alignment: Alignment.topCenter,
-  //           child: GestureDetector(
-  //             onTap: () => Navigator.push(
-  //               context,
-  //               MaterialPageRoute(
-  //                   builder: (context) =>
-  //                       UserCardPage(card: card)),
-  //             ),
-  //             child: Container(
-  //               decoration: BoxDecoration(
-  //                 borderRadius: BorderRadius.circular(10),
-  //                 color:
-  //                     CardView.themes[card.theme]?.background,
-  //                 boxShadow: [
-  //                   BoxShadow(
-  //                     color: Colors.grey.withOpacity(0.15),
-  //                     blurRadius: 8,
-  //                     spreadRadius: 6,
-  //                     offset: const Offset(0, 0),
-  //                   ),
-  //                 ],
-  //               ),
-  //               child: Padding(
-  //                 padding: const EdgeInsets.all(5),
-  //                 child: CardView(card: card),
-  //               ),
-  //             ),
-  //           ),
-  //         );
-  //       },
-  //     );
-  //   },
-  // ).toList(),
-  //           ),
-  //         ),
-  //       ),
-  //     ],
-  //   ),
-  // ),
 
   /// Creates the list of Card Models.
   static buildCardList(BuildContext context) {
-    final double containerWidth = MediaQuery.of(context).size.width * 0.9;
+    final double containerWidth = MediaQuery.of(context).size.width * 0.8;
 
     return context.watch<Cards>().personalcards.map(
       (card) {
@@ -118,14 +60,14 @@ class CardStackState extends State<CardStack> {
                     builder: (context) => UserCardPage(card: card)),
               ),
               child: Padding(
-                padding: const EdgeInsets.only(bottom: 300),
+                padding: const EdgeInsets.only(top: 20),
                 child: Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: CardView.themes[card.theme]?.background,
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(0.15),
+                        color: Colors.black.withOpacity(0.2),
                         blurRadius: 8,
                         spreadRadius: 6,
                         offset: const Offset(0, 0),
