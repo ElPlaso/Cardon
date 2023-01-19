ed720eeb24ce7b9e9517393c5912c4141d5b686e
Moo Plah Soe Kusay <kusaymoo@ecs.vuw.ac.nz>
2023-01-20 01:11:20 +1300

    collected_card_list: update wallet wheel.
    change physics parent to bouncing. extend height. round corners and
    add box shadow to cards. make app bar transparent.

--- lib/pages/wallet.dart
+++ lib/pages/wallet.dart
@@ -1,6 +1,7 @@
+import 'package:cardonapp/pages/scan.dart';
+import 'package:cardonapp/widgets/tapped_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cardonapp/widgets/floater.dart';
 import '../providers/query_provider.dart';
 import '../widgets/wallet_wheel.dart';
 
@@ -10,27 +11,60 @@ class Wallet extends StatelessWidget {
   const Wallet({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Wallet'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const WalletWheel(),
-
-              /// * This button sends a refresh request to the Database for the
-              /// * most modern version of the QRCards stored in the users wallet.
-              Floater(
-                text: 'Refresh Wallet',
-                onClicked: () async =>
-                    {await context.read<QueryProvider>().updateWallet(context)},
-                icon: const Icon(Icons.refresh, size: 25),
+  Widget build(BuildContext context) => Container(
+        color: Colors.grey[50],
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              backgroundColor: Colors.transparent,
+              title: const Text(
+                "Wallet.",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
-            ],
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 13),
+                child: TappedTextButton(
+                  text: "",
+                  iconData: Icons.chevron_left,
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              foregroundColor: Theme.of(context).colorScheme.primary,
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  RefreshIndicator(
+                    onRefresh: () async {
+                      _refreshPage(context);
+                    },
+                    child: const WalletWheel(),
+                  ),
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              // Floating action button on Scaffold.
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Scan()),
+              ),
+              child: const Icon(
+                Icons.crop_free_outlined,
+                size: 35,
+              ),
+            ),
           ),
         ),
       );
+
+  _refreshPage(BuildContext context) async {
+    await context.read<QueryProvider>().updateWallet(context);
+  }
 }
--- lib/widgets/wallet_wheel.dart
+++ lib/widgets/wallet_wheel.dart
@@ -20,39 +20,63 @@ class WalletWheelState extends State<WalletWheel> {
   final _scrollController = FixedExtentScrollController();
 
   @override
-  Widget build(BuildContext context) => SizedBox(
-      height: 500,
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height - 100,
       child: ClickableListWheelScrollView(
-          itemCount: context.read<Cards>().collectedcards.length,
-
-          // When a card in the list is tapped, go to CardPage for that card.
-          onItemTapCallback: (index) {
+        itemCount: context.read<Cards>().collectedcards.length,
+        // When a card in the list is tapped, go to CardPage for that card.
+        onItemTapCallback: (index) {
+          if (index >= 0 &&
+              index < context.read<Cards>().collectedcards.length) {
             Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) => CardPage(
                       card: context.read<Cards>().collectedcards[index])),
             );
-          },
-          itemHeight: 200,
-          scrollController: _scrollController,
-          child: ListWheelScrollView(
-            controller: _scrollController,
-            itemExtent: 200,
-            physics: const FixedExtentScrollPhysics(),
-            onSelectedItemChanged: (index) => {},
-
-            // Get scanned cards and create a list of CardViews to be displayed.
-            children: context.watch<Cards>().collectedcards.map(
-              (card) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return CardView(
-                      card: card,
-                    );
-                  },
-                );
-              },
-            ).toList(),
-          )));
+          }
+        },
+        itemHeight: 200,
+        scrollController: _scrollController,
+        child: ListWheelScrollView(
+          renderChildrenOutsideViewport: true,
+          clipBehavior: Clip.none,
+          controller: _scrollController,
+          itemExtent: 200,
+          physics:
+              const FixedExtentScrollPhysics(parent: BouncingScrollPhysics()),
+          onSelectedItemChanged: (index) => {},
+          perspective: 0.001,
+          // Get scanned cards and create a list of CardViews to be displayed.
+          children: context.watch<Cards>().collectedcards.map(
+            (card) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 8,
+                          spreadRadius: 6,
+                          offset: const Offset(0, 0),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                      color: CardView.themes[card.theme]?.background,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: CardView(card: card),
+                    ),
+                  );
+                },
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
 }
