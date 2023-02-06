609b5e36105fefca2840604db1680b35e913c327
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2023-02-06 23:25:57 +1300

    app: remove unneeded safe area.
    so that all the pages look consistent.

--- lib/user_cards/user_cards.dart
+++ lib/user_cards/user_cards.dart
@@ -11,75 +11,73 @@ class UserCards extends StatelessWidget {
   const UserCards({super.key});
 
   @override
-  Widget build(BuildContext context) => SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            scrolledUnderElevation: 5,
-            elevation: 0,
-            backgroundColor: Colors.grey[50],
-            title: const Text(
-              'Your Cards.',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 13),
-              child: TappedTextButton(
-                text: '',
-                iconData: Icons.chevron_left,
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            foregroundColor: Theme.of(context).colorScheme.primary,
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          scrolledUnderElevation: 5,
+          elevation: 0,
+          backgroundColor: Colors.grey[50],
+          title: const Text(
+            'Your Cards.',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
-          body: context.watch<CardProvider>().isEmpty(true)
-              ? Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.browser_not_supported,
-                        size: 50,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 13),
+            child: TappedTextButton(
+              text: '',
+              iconData: Icons.chevron_left,
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          foregroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        body: context.watch<CardProvider>().isEmpty(true)
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.browser_not_supported,
+                      size: 50,
+                    ),
+                    Text(
+                      'No Cards',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        'No Cards',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              : SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  child: CupertinoScrollbar(
-                    thumbVisibility: true,
-                    thickness: 5,
-                    radius: const Radius.circular(20),
-                    child: SingleChildScrollView(
-                      physics: const BouncingScrollPhysics(
-                        parent: AlwaysScrollableScrollPhysics(),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [SizedBox(height: 20), CardStack()],
-                      ),
+                    ),
+                  ],
+                ),
+              )
+            : SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: CupertinoScrollbar(
+                  thumbVisibility: true,
+                  thickness: 5,
+                  radius: const Radius.circular(20),
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics(),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [SizedBox(height: 20), CardStack()],
                     ),
                   ),
                 ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AddCard()),
-            ),
-            child: const Icon(
-              Icons.library_add,
-              size: 25,
-            ),
+              ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddCard()),
+          ),
+          child: const Icon(
+            Icons.library_add,
+            size: 25,
           ),
         ),
       );
